bbdf96907d00a27d6aa77669514204cfbb9daff6
olive <oliversong7h@gmail.com>
2022-09-23 01:01:32 -0400

    Update db assets

--- app/src/main/java/sam/g/trackuriboh/utils/Constants.kt
+++ app/src/main/java/sam/g/trackuriboh/utils/Constants.kt
@@ -6,6 +6,6 @@ const val DATABASE_PAGE_SIZE = 30
 
 val DATABASE_ASSET_CREATION_DATE = with(Calendar.getInstance(TimeZone.getTimeZone("America/New_York"))) {
     // MONTH IS 0-indexed
-    set(2022, 8, 3, 0, 0)
+    set(2022, 8, 20, 0, 0)
     timeInMillis
 }
