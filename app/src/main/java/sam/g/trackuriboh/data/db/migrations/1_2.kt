d3edf8100a87f05fb55b72931b1e94aac18c8bd9
olive <oliversong7h@gmail.com>
2022-09-24 03:23:55 -0400

    Fix db migration

--- app/src/main/java/sam/g/trackuriboh/data/db/migrations/1_2.kt
+++ app/src/main/java/sam/g/trackuriboh/data/db/migrations/1_2.kt
@@ -10,6 +10,6 @@ val MIGRATION_1_2 = object : Migration(1, 2) {
         database.execSQL("DROP TABLE `CardRarity`")
         database.execSQL("CREATE TABLE `CardRarity` (`id` INTEGER NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY(`id`))")
         database.execSQL("CREATE TABLE `CardSet` (`id` INTEGER NOT NULL, `name` TEXT, `code` TEXT, `releaseDate` INTEGER, `modifiedDate` INTEGER, PRIMARY KEY(`id`))")
-        database.execSQL("CREATE TABLE `Product` (`id` INTEGER NOT NULL, `name` TEXT NOT NULL, `cleanName` TEXT NOT NULL, `type` TEXT NOT NULL, `imageUrl` TEXT, `setId` INTEGER, `number` TEXT, `rarityId` INTEGER, `attribute` TEXT, `cardType` TEXT, `attack` INTEGER, `defense` INTEGER, `description` TEXT, PRIMARY KEY(`id`), FOREIGN KEY(`setId`) REFERENCES `CardSet`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE)")
+        database.execSQL("CREATE TABLE `Product` (`id` INTEGER NOT NULL, `name` TEXT NOT NULL, `cleanName` TEXT NOT NULL, `type` TEXT NOT NULL, `imageUrl` TEXT, `setId` INTEGER, `number` TEXT, `rarityId` INTEGER, `attribute` TEXT, `cardType` TEXT, `attack` INTEGER, `defense` INTEGER, `description` TEXT, PRIMARY KEY(`id`), FOREIGN KEY(`setId`) REFERENCES `CardSet`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE , FOREIGN KEY(`rarityId`) REFERENCES `CardRarity`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE )")
     }
 }
